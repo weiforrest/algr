f2a3d40473a5fc7f9e122ef5b5d8db9e866f1d03
weiforrest <weiforrest@gmail.com>
2019-08-14 00:45:15 +0800

    更新数学基础课第五章的体会

--- Math_Lesson/05_get_rewards.cpp
+++ Math_Lesson/05_get_rewards.cpp
@@ -1,6 +1,9 @@
 /*
  * 程序员的数学基础课 第五讲 递归 
  * 奖赏的所有种类
+ * 
+ * 使用递归保存所需要的中间状态和变量，减少编程时所需要负担的变量
+ * 递归的核心思想和数学归纳法类似，将当前问题化为两部分：一个当前所需要解决的问题和另一个更简单的问题
 */
 #include <iostream>
 #include <vector>
@@ -20,17 +23,18 @@ void print_result(vector<int> result)
 
 void get(int sum, vector<int> result)
 {
+    // 结束，得到满足条件的解。
     if (sum == 0){
         print_result(result);
         return ;
-    } else if(sum < 0){
+    } else if(sum < 0){ // 终止计算
         return;
-    }else{
+    }else{  // 假设已知k=n-1的解，求k=n时的解
         for(int i = 0; i < 4; i++){
             vector<int> newresult;
-            newresult.assign(result.begin(), result.end());
+            newresult.assign(result.begin(), result.end()); // 每一个新的结果都复制上一次的结果。
             newresult.push_back(rewards[i]);
-            get(sum - rewards[i], newresult);
+            get(sum - rewards[i], newresult);   //k=n-1的解，留给嵌套
         }
     }
 
